9250a88d84a6d3250a7d1b48973c8a3f1d7f1429
bruce <cnhxunoo@gmail.com>
2025-03-01 21:15:20 +0800

    chore: improve header

--- src/renderer/src/components/layout/Header.tsx
+++ src/renderer/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { MenuIcon, UserIcon } from "lucide-react";
+import { ChartNoAxesCombined, Edit, MenuIcon, User, UserIcon } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
 
 interface SingleMenuProps {
@@ -17,7 +17,7 @@ function SingleMenu(props: SingleMenuProps) {
       } ${props.className && props.className}`}
     >
       {props.icon}
-      <Link to={props.link} className="font-semibold text-xl">
+      <Link to={props.link} className="text-xl">
         {props.label}
       </Link>
     </div>
@@ -26,14 +26,23 @@ function SingleMenu(props: SingleMenuProps) {
 
 export default function Header() {
   return (
-    <header className="w-full h-20 bg-white shadow-sm">
-      <nav className="navbar h-full py-0  text-base-content">
-        <div className="navbar-start">logo</div>
-        <div className="navbar-center h-full gap-2">
-          <SingleMenu icon={<MenuIcon className="size-7" />} label="管理中心" link="/manage" />
-          <SingleMenu icon={<UserIcon className="size-7" />} label="账号中心" link="/account" />
+    <header className="w-full h-20 bg-white shadow-sm px-2">
+      <nav className="navbar h-full py-0  text-base-content/50">
+        <div className="navbar-start gap-5">
+          <ChartNoAxesCombined size="40" className="text-primary" />
+          <p className="border-l-2 border-base-content/30 pl-5 text-md">1388888888</p>
         </div>
-        <div className="navbar-end">account manager</div>
+        <div className="navbar-center h-full gap-5">
+          <SingleMenu icon={<MenuIcon size="20" />} label="管理中心" link="/manage" />
+          <SingleMenu icon={<UserIcon size="20" />} label="账号中心" link="/account" />
+          <SingleMenu icon={<Edit size="20" />} label="一键发布" link="/publish" />
+        </div>
+        <div className="navbar-end gap-7">
+          <button type="button">开通VIP</button>
+          <button type="button">
+            <User size="27" className="text-primary/70" />
+          </button>
+        </div>
       </nav>
     </header>
   );
